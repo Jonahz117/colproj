e9510da555b35b1c13d98fa4bdd3559db4e3862f
Jonahz117 <Jonahzdelapaz117@gmail.com>
2025-09-04 19:44:10 +0800

    Add files via upload

---
+++ sumavemax.py
@@ -0,0 +1,15 @@
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+num3 = float(input("Enter the third number: "))
+
+# Add the numbers
+result = num1 + num2 + num3
+average = (num1 + num2 + num3) / 3
+
+
+
+
+# Show the result
+print("The sum is:", result)
+print("The ave is:", average)
+print(max(num1, num2, num3))
